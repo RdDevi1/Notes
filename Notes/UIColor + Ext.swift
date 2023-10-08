d0b5ae86536e57d81b5b777416689012c160c9d3
Sbat2000 <zevzok@gmail.com>
2023-10-08 22:51:58 +0800

    add color and image extension

---
+++ Notes/UIColor + Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor + Ext.swift
+//  Notes
+//
+//  Created by Aleksandr Garipov on 08.10.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    public static let primary = #colorLiteral(red: 1, green: 0.7006493211, blue: 0.276804626, alpha: 1)
+    public static let Error = #colorLiteral(red: 0.8807182312, green: 0.4922047853, blue: 0.4225139618, alpha: 1)
+}
